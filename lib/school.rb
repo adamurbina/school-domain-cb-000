bae5978d3e39cda582b1caf905fb80f63c9cd77e
Adam Urbina <adamurbina@gmail.com>
2018-06-06 10:56:13 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+
+    def initialize
+        roster = {}
+    end
+
+    def add_student(name, grade)
+        roster[grade] = [] if !roster.keys.include?(grade)
+        roster[grade] << name
+    end
+
+end
